7736efd67eeb7f096fa7a014d43c1a8b56edaa69
PrathamNabira <prathamnabira10@gmail.com>
2025-07-20 20:03:00 +0530

    this keyword

---
+++ this.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <ostream>
+class entity{
+  public:
+    int x , y;
+    entity(int x , int y){
+      this->x=x;
+      (*this).y=y;
+    }
+};
+std::ostream& operator<<(std::ostream& stream , entity& e){
+  std::cout<<e.x<<", "<<e.y;
+  return stream;
+}
+int main(){
+  entity e(1,2);
+  std::cout<<e<<std::endl;
+}
