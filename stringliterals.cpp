7cf38de8ce8a13de3e8b56388c5820870a80f1f5
PrathamNabira <prathamnabira10@gmail.com>
2025-07-20 20:01:57 +0530

    string literals

---
+++ stringliterals.cpp
@@ -0,0 +1,11 @@
+#include <cstring>
+#include <iostream>
+int main(){
+  const char* name = "jo\0hn";
+std::cout<<strlen(name)<<std::endl;
+const char* name0 = "pratham";
+  const wchar_t* name1= L"pratham";
+  const char16_t* name2= u"pratham";
+  const char32_t* name3= U"pratham";
+}
+
