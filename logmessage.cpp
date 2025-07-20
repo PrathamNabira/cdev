c99884aedfc4453f6c2e843819ca5741ec6b2d04
PrathamNabira <prathamnabira10@gmail.com>
2025-07-20 19:56:32 +0530

    log messages with enums and member initialization

---
+++ logmessage.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+class log{
+  public:
+    enum level:unsigned char{
+      en_error=0,en_warning,en_info
+    };
+  private:
+    level m_loglevel=en_info;
+  public:
+    void setlevel(level level){
+       m_loglevel = level;
+    }
+    void error(const char* message){
+      if (m_loglevel>=en_error) {
+      std::cout<<"[error:]"<<message<<std::endl;
+      }
+    }
+    void warn(const char* message){
+      if (m_loglevel>=en_warning) {
+      std::cout<<"[warning:]"<<message<<std::endl;
+      }
+    }
+    void info(const char* message){
+      if (m_loglevel>=en_info) {
+      std::cout<<"[info:]"<<message<<std::endl;
+      }
+    }
+};
+int main(){
+  log log;
+  log.setlevel(log::en_info);
+  log.error("hello!");
+  log.warn("hello!");
+  log.info("hello!");
+}
---
+++ memberinit.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <string>
+class example{
+  public:
+    example(){
+      std::cout<<"example created!"<<std::endl;
+    }
+    example(int x){
+      std::cout<<"example created with "<< x <<'!'<<std::endl;
+    }
+};
+class entity{
+  private:
+    std::string m_name;
+    example m_example;
+  public:
+    // entity(int a):m_example(example(a)){}
+    entity(int a):m_example(a){}
+    entity():m_name("unknown"){};
+    entity(const std::string& name):m_name(name){
+    }
+    const std::string& getname(){return m_name;}
+};
+int main(){
+  entity e0(8);
+  std::cout<<e0.getname()<<std::endl; 
+}
