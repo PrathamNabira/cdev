e8dd0ab60826b65b73cb79a7470161d7c21e8fd6
PrathamNabira <prathamnabira10@gmail.com>
2025-07-22 22:13:56 +0530

    std::vector

--- std::vector.cpp
+++ std::vector.cpp
@@ -4,26 +4,21 @@
 
 struct vertex {
   float x,y,z;
+  vertex(float x, float y, float z):x(x),y(y),z(z){};
+  vertex(const vertex& vertex):x(vertex.x),y(vertex.y),z(vertex.z){
+    std::cout<<"compied"<<std::endl;
+  };
 };
 
 std::ostream& operator<<(std::ostream& ostream , vertex& vertex){
   std::cout<<vertex.x<<", "<<vertex.y<<", "<<vertex.z;
   return ostream;
 }
-void function(const std::vector<vertex>& vertices){
-  
-}
 
 int main(){
   std::vector<vertex>vertices;
-  function(vertices);
-  vertices.push_back({1,2,3});
-  vertices.push_back({3,4,5});
-  for (int i =0 ; i<vertices.size(); i++) {
-    std::cout<<vertices[i]<<std::endl;
-  }
-  vertices.erase(vertices.begin()+1);
-  for (vertex& v : vertices) {
-    std::cout<<v<<std::endl;
-  }
+  vertices.reserve(3);
+  vertices.emplace_back(1,2,3);
+  vertices.emplace_back(4,5,6);
+  vertices.emplace_back(7,8,9);
 }
