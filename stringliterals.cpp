931840ec66f5376c1f90e2572a1825909f25b3b5
PrathamNabira <prathamnabira10@gmail.com>
2025-07-24 20:04:56 +0530

    indentation

--- stringliterals.cpp
+++ stringliterals.cpp
@@ -4,8 +4,8 @@ int main(){
   const char* name = "jo\0hn";
 std::cout<<strlen(name)<<std::endl;
 const char* name0 = "pratham";
-  const wchar_t* name1= L"pratham";
-  const char16_t* name2= u"pratham";
-  const char32_t* name3= U"pratham";
+const wchar_t* name1= L"pratham";
+const char16_t* name2= u"pratham";
+const char32_t* name3= U"pratham";
 }
 
